76864e2a5b89f395c409824d80dab5a7289e6d8a
Noah Delagardelle <noahcdela@gmail.com>
2018-01-18 22:45:08 -0600

    init

---
+++ js/functions.js
@@ -0,0 +1,79 @@
+$(function() {
+
+    $('.bubble-0').click(function() {
+        $('.haiku-0').addClass('haiku-open');
+    });
+
+    $('.bubble-12').click(function() {
+        $('.haiku-12').addClass('haiku-open');
+    });
+
+    $('.bubble-20').click(function() {
+        $('.haiku-20').addClass('haiku-open');
+    });
+
+    $('.bubble-4').click(function() {
+        $('.haiku-4').addClass('haiku-open');
+    });
+
+    $('.bubble-1').click(function() {
+        $('.haiku-1').addClass('haiku-open');
+    });
+
+    $('.bubble-8').click(function() {
+        $('.haiku-8').addClass('haiku-open');
+    });
+
+    $('.bubble-10').click(function() {
+        $('.haiku-10').addClass('haiku-open');
+    });
+
+    $('.bubble-5').click(function() {
+        $('.haiku-5').addClass('haiku-open');
+    });
+
+////
+
+    $('.haiku-0-link').click(function() {
+        $('.video-0').addClass('video-open');
+    });
+
+    $('.haiku-12-link').click(function() {
+        $('.video-12').addClass('video-open');
+    });
+
+    $('.haiku-20-link').click(function() {
+        $('.video-20').addClass('video-open');
+    });
+
+    $('.haiku-4-link').click(function() {
+        $('.video-4').addClass('video-open');
+    });
+
+    $('.haiku-1-link').click(function() {
+        $('.video-1').addClass('video-open');
+    });
+
+    $('.haiku-8-link').click(function() {
+        $('.video-8').addClass('video-open');
+    });
+
+    $('.haiku-10-link').click(function() {
+        $('.video-10').addClass('video-open');
+    });
+
+    $('.haiku-5-link').click(function() {
+        $('.video-5').addClass('video-open');
+    });
+
+
+    ////
+
+    $('.back-btn').click(function() {
+        $('.haiku-page').removeClass('haiku-open');
+        $('.video-container').removeClass('video-open');
+    })
+
+
+
+});
